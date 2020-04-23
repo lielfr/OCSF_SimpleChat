9a5fbae32d703d3060b12b7c6830d052d4912cfb
Liel Fridman <lielft@gmail.com>
2020-04-23 14:28:06 +0300

    Initial commit

---
+++ src/main/java/il/ac/haifa/cs/sweng/OCSFSimpleChat/SimpleChatServer.java
@@ -0,0 +1,34 @@
+package il.ac.haifa.cs.sweng.OCSFSimpleChat;
+
+import java.io.IOException;
+
+import il.ac.haifa.cs.sweng.OCSFSimpleChat.ocsf.server.AbstractServer;
+import il.ac.haifa.cs.sweng.OCSFSimpleChat.ocsf.server.ConnectionToClient;
+
+public class SimpleChatServer extends AbstractServer {
+
+	public SimpleChatServer(int port) {
+		super(port);
+	}
+
+	@Override
+	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+		System.out.println("Received Message: " + msg.toString());
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sendToAllClients(msg);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.out.println("Required argument: <port>");
+		} else {
+			SimpleChatServer server = new SimpleChatServer(Integer.parseInt(args[0]));
+			server.listen();
+		}
+	}
+}
